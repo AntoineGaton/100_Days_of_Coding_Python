53ea094fa443ab0a611d56e2ec2cf7127eb97165
Antoine Gaton <antoinegaton@gmail.com>
2024-01-31 17:13:58 -0500

    100 day python bootcamp

---
+++ .history/Day_1/input_function_20240131165901.py
@@ -0,0 +1,12 @@
+# Provide any name in the input pane below.
+# That value can be accessed using the input() function.
+# Don't put anything inside the input() function!
+def num_letters(name):
+  # count = 0
+  # for i in name:
+  #   count+=1
+  print(count)
+
+name = input()
+
+num_letters(name)
---
+++ .history/Day_2/bmi_calculator_20240131171152.py
@@ -0,0 +1,13 @@
+# 1st input: enter height in meters e.g: 1.65
+height = input()
+# 2nd input: enter weight in kilograms e.g: 72
+weight = input()
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+height = float(height)
+weight = int(weight)
+
+# bmi = int(weight/(height*height))
+bmi = weight/height**2
+print(bmi)
